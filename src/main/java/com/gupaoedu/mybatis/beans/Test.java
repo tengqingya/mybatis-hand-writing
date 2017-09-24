bbf246a8be330abe581941bf094c548c868c9e57
tengqingya <12345tengqingya>
2017-09-24 12:18:24 +0800

    修复报错

--- src/main/java/com/gupaoedu/mybatis/beans/Test.java
+++ src/main/java/com/gupaoedu/mybatis/beans/Test.java
@@ -11,7 +11,31 @@ public class Test {
 
     private String name;
 
-    public Test(Integer id, Integer nums, String name) {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId( Integer id ) {
+        this.id = id;
+    }
+
+    public Integer getNums() {
+        return nums;
+    }
+
+    public void setNums( Integer nums ) {
+        this.nums = nums;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public Test( Integer id, Integer nums, String name) {
         this.id = id;
         this.nums = nums;
         this.name = name;
